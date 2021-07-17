5e1718f9603b407ea1c546462da220c9551645bf
David Shlemayev <david.shlemayev@gmail.com>
2021-07-17 12:51:24 +0300

    Make `dir_*` be methods

--- src/ipc.rs
+++ src/ipc.rs
@@ -20,7 +20,7 @@ impl Debug for IpcRef {
     fn fmt(&self, f: &mut Formatter<'_>) -> core::fmt::Result {
         match self.inner.as_ref() {
             IpcNode::Dir(_) => write!(f, "IpcNode::Dir#{}", self.id),
-            IpcNode::Stream => write!(f, "IpcNode::Stream#{}", self.id),
+            IpcNode::Pipe => write!(f, "IpcNode::Pipe#{}", self.id),
             IpcNode::Blob => write!(f, "IpcNode::Blob#{}", self.id),
             IpcNode::Endpoint => write!(f, "IpcNode::Endpoint#{}", self.id),
         }
@@ -29,7 +29,7 @@ impl Debug for IpcRef {
 
 enum IpcNode {
     Dir(IpcDir),
-    Stream,
+    Pipe,
     Blob,
     Endpoint,
 }
@@ -38,17 +38,17 @@ struct IpcDir {
     entries: RwLock<Vec<IpcRef>>,
 }
 
-impl IpcNode {
-    pub fn dir_list<'a>(this: Arc<IpcNode>) -> Option<BoxStream<'a, IpcRef>> {
-        if !matches!(this.as_ref(), IpcNode::Dir(_)) {
+impl IpcRef {
+    pub fn dir_list(&self) -> Option<BoxStream<IpcRef>> {
+        if !matches!(self.inner.as_ref(), IpcNode::Dir(_)) {
             return None;
         }
         Some(Box::pin(stream::unfold(0, move |idx| {
-            let this = this.clone(); // Eww
+            let node = self.inner.clone(); // Eww
 
             async move {
-                let this = unwrap_variant!(this.as_ref(), IpcNode::Dir);
-                let entries = this.entries.read();
+                let dir = unwrap_variant!(node.as_ref(), IpcNode::Dir);
+                let entries = dir.entries.read();
 
                 if idx < entries.len() {
                     Some((
@@ -64,80 +64,82 @@ impl IpcNode {
         })))
     }
 
-    pub fn dir_get<'a>(this: Arc<IpcNode>, id: u64) -> BoxFuture<'a, Option<IpcRef>> {
-        if !matches!(this.as_ref(), IpcNode::Dir(_)) {
+    pub fn dir_get(&self, id: u64) -> BoxFuture<Option<IpcRef>> {
+        if !matches!(self.inner.as_ref(), IpcNode::Dir(_)) {
             return Box::pin(async { None });
         }
         Box::pin(async move {
-            let this = unwrap_variant!(this.as_ref(), IpcNode::Dir);
-            let entries = this.entries.read();
-
+            let dir = unwrap_variant!(self.inner.as_ref(), IpcNode::Dir);
+            let entries = dir.entries.read();
             entries.iter().find(|e| e.id == id).cloned()
         })
     }
 }
 
 pub fn init() {
-    ROOT.write().replace(Arc::new(IpcNode::Dir(IpcDir {
-        entries: RwLock::new(vec![
-            IpcRef {
-                id: 1,
-                inner: Arc::new(IpcNode::Dir(IpcDir {
-                    entries: RwLock::new(vec![
-                        IpcRef {
-                            id: 1,
-                            inner: Arc::new(IpcNode::Dir(IpcDir {
-                                entries: RwLock::new(vec![]),
-                            })),
-                        },
-                        IpcRef {
-                            id: 2,
-                            inner: Arc::new(IpcNode::Dir(IpcDir {
-                                entries: RwLock::new(vec![]),
-                            })),
-                        },
-                        IpcRef {
-                            id: 3,
-                            inner: Arc::new(IpcNode::Dir(IpcDir {
-                                entries: RwLock::new(vec![]),
-                            })),
-                        },
-                    ]),
-                })),
-            },
-            IpcRef {
-                id: 2,
-                inner: Arc::new(IpcNode::Dir(IpcDir {
-                    entries: RwLock::new(vec![]),
-                })),
-            },
-            IpcRef {
-                id: 3,
-                inner: Arc::new(IpcNode::Dir(IpcDir {
-                    entries: RwLock::new(vec![]),
-                })),
-            },
-            IpcRef {
-                id: 4,
-                inner: Arc::new(IpcNode::Dir(IpcDir {
-                    entries: RwLock::new(vec![]),
-                })),
-            },
-        ]),
-    })));
+    ROOT.write().replace(IpcRef {
+        id: 0,
+        inner: Arc::new(IpcNode::Dir(IpcDir {
+            entries: RwLock::new(vec![
+                IpcRef {
+                    id: 1,
+                    inner: Arc::new(IpcNode::Dir(IpcDir {
+                        entries: RwLock::new(vec![
+                            IpcRef {
+                                id: 1,
+                                inner: Arc::new(IpcNode::Dir(IpcDir {
+                                    entries: RwLock::new(vec![]),
+                                })),
+                            },
+                            IpcRef {
+                                id: 2,
+                                inner: Arc::new(IpcNode::Dir(IpcDir {
+                                    entries: RwLock::new(vec![]),
+                                })),
+                            },
+                            IpcRef {
+                                id: 3,
+                                inner: Arc::new(IpcNode::Dir(IpcDir {
+                                    entries: RwLock::new(vec![]),
+                                })),
+                            },
+                        ]),
+                    })),
+                },
+                IpcRef {
+                    id: 2,
+                    inner: Arc::new(IpcNode::Dir(IpcDir {
+                        entries: RwLock::new(vec![]),
+                    })),
+                },
+                IpcRef {
+                    id: 3,
+                    inner: Arc::new(IpcNode::Dir(IpcDir {
+                        entries: RwLock::new(vec![]),
+                    })),
+                },
+                IpcRef {
+                    id: 4,
+                    inner: Arc::new(IpcNode::Dir(IpcDir {
+                        entries: RwLock::new(vec![]),
+                    })),
+                },
+            ]),
+        })),
+    });
 }
 
 pub async fn test() {
     let ipc_dir = ROOT.read().as_ref().unwrap().clone();
-    let mut stream = IpcNode::dir_list(ipc_dir.clone()).unwrap();
+    let mut stream = ipc_dir.dir_list().unwrap();
     while let Some(item) = stream.next().await {
         println!("{:?}", item);
-        let mut inner_stream = IpcNode::dir_list(item.inner.clone()).unwrap();
+        let mut inner_stream = item.dir_list().unwrap();
         while let Some(inner_item) = inner_stream.next().await {
             println!("- {:?}", inner_item);
         }
     }
-    println!("{:?}", IpcNode::dir_get(ipc_dir, 0).await.unwrap());
+    println!("{:?}", ipc_dir.dir_get(1).await.unwrap());
 }
 
-static ROOT: RwLock<Option<Arc<IpcNode>>> = RwLock::new(None);
+static ROOT: RwLock<Option<IpcRef>> = RwLock::new(None);
