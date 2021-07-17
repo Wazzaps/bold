2bbafd65c7a7aac31e368ab06db1772234fc03b8
David Shlemayev <david.shlemayev@gmail.com>
2021-07-17 12:32:45 +0300

    More realistic `IpcNode::Dir`

--- src/ipc.rs
+++ src/ipc.rs
@@ -1,78 +1,145 @@
 use crate::println;
+use crate::unwrap_variant;
 use alloc::boxed::Box;
 use alloc::sync::{Arc, Weak};
+use alloc::vec;
 use alloc::vec::Vec;
 use core::fmt::{Debug, Formatter};
+use core::future::Future;
 use core::pin::Pin;
+use futures::future::BoxFuture;
 use futures::stream::BoxStream;
 use futures::{stream, Stream, StreamExt};
 use spin::RwLock;
 
+#[derive(Clone)]
 struct IpcRef {
     id: u64,
-    inner: Weak<Node>,
+    inner: Arc<IpcNode>,
 }
 
 impl Debug for IpcRef {
     fn fmt(&self, f: &mut Formatter<'_>) -> core::fmt::Result {
-        write!(f, "IpcRef(id: {})", self.id)
+        match self.inner.as_ref() {
+            IpcNode::Dir(_) => write!(f, "IpcNode::Dir#{}", self.id),
+            IpcNode::Stream => write!(f, "IpcNode::Stream#{}", self.id),
+            IpcNode::Blob => write!(f, "IpcNode::Blob#{}", self.id),
+            IpcNode::Endpoint => write!(f, "IpcNode::Endpoint#{}", self.id),
+        }
     }
 }
 
-enum Node {
-    IpcDir(IpcDir),
-    IpcStream,
-    IpcBlob,
+enum IpcNode {
+    Dir(IpcDir),
+    Stream,
+    Blob,
+    Endpoint,
 }
 
 struct IpcDir {
-    example_count: usize,
-    // entries: RwLock<Vec<IpcRef>>
+    entries: RwLock<Vec<IpcRef>>,
 }
 
-impl Node {
-    pub fn list<'a>(this: Arc<Node>) -> BoxStream<'a, IpcRef> {
-        Box::pin(stream::unfold(0, move |id| {
+impl IpcNode {
+    pub fn dir_list<'a>(this: Arc<IpcNode>) -> Option<BoxStream<'a, IpcRef>> {
+        if !matches!(this.as_ref(), IpcNode::Dir(_)) {
+            return None;
+        }
+        Some(Box::pin(stream::unfold(0, move |idx| {
             let this = this.clone(); // Eww
 
             async move {
-                let this = if let Node::IpcDir(this) = this.as_ref() {
-                    this
-                } else {
-                    panic!();
-                };
+                let this = unwrap_variant!(this.as_ref(), IpcNode::Dir);
+                let entries = this.entries.read();
 
-                if id < this.example_count {
+                if idx < entries.len() {
                     Some((
                         // Yield this
-                        IpcRef {
-                            id: id as u64,
-                            inner: Weak::new(),
-                        },
+                        entries[idx].clone(),
                         // Next state
-                        id + 1,
+                        idx + 1,
                     ))
                 } else {
                     None
                 }
             }
-        }))
+        })))
+    }
+
+    pub fn dir_get<'a>(this: Arc<IpcNode>, id: u64) -> BoxFuture<'a, Option<IpcRef>> {
+        if !matches!(this.as_ref(), IpcNode::Dir(_)) {
+            return Box::pin(async { None });
+        }
+        Box::pin(async move {
+            let this = unwrap_variant!(this.as_ref(), IpcNode::Dir);
+            let entries = this.entries.read();
+
+            entries.iter().find(|e| e.id == id).cloned()
+        })
     }
 }
 
 pub fn init() {
-    ROOT.write().replace(Arc::new(Node::IpcDir(IpcDir {
-        // entries: RwLock::new(vec![]),
-        example_count: 6,
+    ROOT.write().replace(Arc::new(IpcNode::Dir(IpcDir {
+        entries: RwLock::new(vec![
+            IpcRef {
+                id: 1,
+                inner: Arc::new(IpcNode::Dir(IpcDir {
+                    entries: RwLock::new(vec![
+                        IpcRef {
+                            id: 1,
+                            inner: Arc::new(IpcNode::Dir(IpcDir {
+                                entries: RwLock::new(vec![]),
+                            })),
+                        },
+                        IpcRef {
+                            id: 2,
+                            inner: Arc::new(IpcNode::Dir(IpcDir {
+                                entries: RwLock::new(vec![]),
+                            })),
+                        },
+                        IpcRef {
+                            id: 3,
+                            inner: Arc::new(IpcNode::Dir(IpcDir {
+                                entries: RwLock::new(vec![]),
+                            })),
+                        },
+                    ]),
+                })),
+            },
+            IpcRef {
+                id: 2,
+                inner: Arc::new(IpcNode::Dir(IpcDir {
+                    entries: RwLock::new(vec![]),
+                })),
+            },
+            IpcRef {
+                id: 3,
+                inner: Arc::new(IpcNode::Dir(IpcDir {
+                    entries: RwLock::new(vec![]),
+                })),
+            },
+            IpcRef {
+                id: 4,
+                inner: Arc::new(IpcNode::Dir(IpcDir {
+                    entries: RwLock::new(vec![]),
+                })),
+            },
+        ]),
     })));
 }
 
 pub async fn test() {
     let ipc_dir = ROOT.read().as_ref().unwrap().clone();
-    let mut s = Node::list(ipc_dir);
-    while let Some(item) = s.next().await {
+    let mut stream = IpcNode::dir_list(ipc_dir.clone()).unwrap();
+    while let Some(item) = stream.next().await {
         println!("{:?}", item);
+        let mut inner_stream = IpcNode::dir_list(item.inner.clone()).unwrap();
+        while let Some(inner_item) = inner_stream.next().await {
+            println!("- {:?}", inner_item);
+        }
     }
+    println!("{:?}", IpcNode::dir_get(ipc_dir, 0).await.unwrap());
 }
 
-static ROOT: RwLock<Option<Arc<Node>>> = RwLock::new(None);
+static ROOT: RwLock<Option<Arc<IpcNode>>> = RwLock::new(None);
--- src/utils.rs
+++ src/utils.rs
@@ -2,6 +2,22 @@ use crate::println;
 use core::fmt;
 use core::fmt::Write;
 
+#[macro_export]
+macro_rules! unwrap_variant {
+    ($source: expr, $variant: path) => {{
+        if let $variant(val) = $source {
+            val
+        } else {
+            panic!(concat!(
+                "Invalid variant in '",
+                stringify!($source),
+                "', expected ''",
+                stringify!($variant)
+            ));
+        }
+    }};
+}
+
 pub fn display_bstr(fmt: &mut fmt::Formatter<'_>, bstr: &[u8]) -> fmt::Result {
     bstr.iter().try_for_each(|c| fmt.write_char(char::from(*c)))
 }
